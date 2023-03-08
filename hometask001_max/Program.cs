5df45bfb580e0c95dde6c34a23fe91aec3a33d8d
Kostya <kosg@mail.ru>
2023-03-09 01:35:50 +0300

    add max

---
+++ hometask001_max/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число: ");
+int namberA = Convert.ToInt32(value: Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int namberB = Convert.ToInt32(value: Console.ReadLine());
+if(namberA > namberB)
+{
+    Console.Write("max = ");
+    Console.WriteLine(namberA);
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(namberB);   
+}
