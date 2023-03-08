785a1442559507afbd3e2a4f7239dda199feba03
Kostya <kosg@mail.ru>
2023-03-09 01:51:38 +0300

    add max 3

---
+++ hometask002_max3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число: ");
+int namberA = Convert.ToInt32(value: Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int namberB = Convert.ToInt32(value: Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int namberС = Convert.ToInt32(value: Console.ReadLine());
+int max = namberA;
+if(namberB > max) max = namberB;
+if(namberС > max) max = namberС;
+
+Console.Write("max = ");
+Console.WriteLine(max);   
+
