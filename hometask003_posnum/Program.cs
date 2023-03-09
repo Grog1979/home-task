3ddf522486979a23fa00dc2dd9d9fe30e3e6b74e
Kostya <kosg@mail.ru>
2023-03-09 08:18:43 +0300

    add positive

---
+++ hometask003_posnum/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число: ");
+int numberA = Convert.ToInt32(value: Console.ReadLine());
+if(numberA%2 == 0)
+    Console.WriteLine("positiv");
+else
+    Console.WriteLine("negativ");
