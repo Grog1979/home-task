1e9fdd954fc0bd7918f91ed0d5aad51f51c7c4d3
Kostya <kosg@mail.ru>
2023-03-09 08:42:31 +0300

    add num

---
+++ hometask004_num/Program.cs
@@ -0,0 +1,5 @@
+﻿// positive and negative numbers
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(value: Console.ReadLine());
+for(int i = 2; i <= n; i = i + 2)
+    Console.Write($"{i} ");
